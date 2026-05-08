10570dec43862fd58b36c4f0fa3144a85b1ffed3
williamervin7 <williamervin7@gmail.com>
2026-05-08 17:18:58 -0500

    added pytest to requirements.txt added test_model.py place holder

---
+++ test/test_model.py
@@ -0,0 +1,18 @@
+# This file contains tests for the model component of the Healthcare Accessibility Demand Modeling project.
+# The tests are designed to ensure that the model is functioning correctly and producing expected results.
+# Note: The actual test functions are placeholders and should be implemented with specific test cases relevant to the model's functionality.
+# These tests will be run with github CI/CD pipeline to ensure that any changes to the model do not break existing functionality.
+import pytest
+import pandas as pd
+import numpy as np
+import os
+
+# change names later
+def first_test():
+    pass
+
+def second_test():
+    pass
+
+def third_test():
+    pass
